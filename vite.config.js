b44f6200c15b045e024f24538a03aa67e34041c2
shiva3187 <shivansh3187@gmail.com>
2024-08-10 11:06:33 +0530

    add vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      // '/api': 'https://astrabot-backend.vercel.app/api/',
+      // '/api': 'http://localhost:3000',
+      // '/api': 'https://astrabot-backend-aw248tbde-shivansh-kasaudhans-projects.vercel.app/',
+      '/api': 'https://astrabot-backend.vercel.app/'
+      
       
     },
   },
